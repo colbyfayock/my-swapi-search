53f38142c9e4b100dcd8b4430e0caa53cf19ceb5
colbyfayock <fayock@gmail.com>
2021-07-13 19:33:33 -0400

    removing unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
